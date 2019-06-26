263dc93c85d1329345a8f6735cfa6a98a6d631e3
Prateek Chachra <prateekchachra@live.com>
2019-06-27 00:07:05 +0530

    Added Nav Button

--- src/components/common/SetLocation.js
+++ src/components/common/SetLocation.js
@@ -77,6 +77,11 @@ const styles = StyleSheet.create({
       calloutView: {
         backgroundColor: "rgba(255, 255, 255, 0.9)",
         borderRadius: 10,
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
         justifyContent: 'flex-start',
         width: "80%",
         marginLeft: "20%",
--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {TouchableOpacity} from 'react-native';
 import {Scene, Router, Actions} from 'react-native-router-flux';
 
 import ChatWindow from './components/chat/ChatWindow';
@@ -7,9 +8,14 @@ import Profile from './components/profile/Profile';
 import PeopleAroundMe from './components/people/PeopleAroundMe';
 import Messages from './components/messages/Messages';
 import styles from './styles';
+import Icon from 'react-native-vector-icons/Entypo';
 import SetLocation from './components/common/SetLocation';
-const RouterComponent = () => {
+class RouterComponent extends Component{
 
+    state= {
+        optionsTab: ''
+    }
+    render(){
     return(
         <Router>
             <Scene key="root" hideNavBar>
@@ -20,21 +26,23 @@ const RouterComponent = () => {
     </Scene> */}
             <Scene key="main">
             <Scene key="swipeScreen" initial
-            rightButtonTextStyle={styles.navRight}
             leftButtonTextStyle={styles.navLeft}
             titleStyle={styles.navTitle}
-            
                 leftTitle="My Messages"
                 onLeft={() => {
                     Actions.userMessages();
                 }}
-                 rightTitle="Around You"
-                 onRight={() => {
-                    Actions.aroundMe();
-                 }}
+               renderRightButton={() => (<TouchableOpacity>
+                   <Icon name="dots-three-vertical" size={20}/>
+                   </TouchableOpacity>
+               )}
             component={SwipePeople} title="Find A Wing" ></Scene>           
             <Scene key="aroundMe"
              titleStyle={styles.navAroundMe}
+             rightButtonImage={this.state.optionsTab}
+             onRight={() => {
+                Actions.aroundMe();
+             }}
             component={PeopleAroundMe} title="People Around You"></Scene>
 
             <Scene key="chat" 
@@ -57,8 +65,8 @@ const RouterComponent = () => {
         </Router>
 
     );
-
-};
+                }
+}
 
 
 export default RouterComponent;
